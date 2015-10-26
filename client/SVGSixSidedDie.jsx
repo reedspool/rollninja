9149527aeacf6663426605c98daf406d39609734
reedspool <reedmagic@gmail.com>
2015-10-25 23:42:44 -0400

    refactoring

--- client/SVGSixSidedDie.jsx
+++ client/SVGSixSidedDie.jsx
@@ -1,17 +1,30 @@
 SVGSixSidedDie = React.createClass({
   propTypes: {
-    value: React.PropTypes.number.isRequired
+    value: React.PropTypes.number.isRequired,
+    height:  React.PropTypes.string,
+    width:  React.PropTypes.string,
+  },
+
+  getDefaultProps() {
+    return {
+      value: 1,
+      height:  198,
+      width:  210
+    }
   },
 
   render() {
     const value = this.props.value;
+    const height = this.props.height;
+    const width = this.props.width;
 
-    const id = "#die" + (value || 1);
+    const symbolId = "#die" + value;
 
-function createMarkup() { return { __html: '<use xlink:href="' + id + '" />' }; };
+    // Straight from react docs for dangerouslySetInnerHTML
+    function createMarkup() { return { __html: `<use xlink:href="${symbolId}" />` }; };
 
     return (
-      <svg className="die-svg-use" dangerouslySetInnerHTML={createMarkup()} />
+      <svg className="die-svg-use" dangerouslySetInnerHTML={createMarkup()} height={height} width={width} />
     );
   }
 });
